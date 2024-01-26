b3e5c3a22601419f52b74dbf6efd12990701f21c
Franck <franckvienot7@gmail.com>
2024-01-26 15:13:50 +0100

    added State to mobile Navbar

--- components/NavbarMobile.tsx
+++ components/NavbarMobile.tsx
@@ -1,13 +1,31 @@
+"use client"
+import { useState } from "react"
+
 import { HomeIcon, User, SquarePenIcon, Settings2 } from "lucide-react"
 
 export default function NavbarMobile() {
+  const [selectedIcon, setSelectedIcon] = useState<String | null>(null)
+
+  const handleIconClick = (iconName: String) => {
+    setSelectedIcon(iconName)
+  }
+
+  const iconClass = (iconName: String) =>
+    `iconClass ${selectedIcon === iconName ? "text-blue-500" : ""}`
+
   return (
     <>
       <div className="mobileNavbarContainer">
-        <HomeIcon className="iconClass" />
-        <User className="iconClass" />
-        <SquarePenIcon className="iconClass" />
-        <Settings2 className="iconClass" />
+        <HomeIcon className={iconClass("Home")} onClick={() => handleIconClick("Home")} />
+        <User className={iconClass("User")} onClick={() => handleIconClick("User")} />
+        <SquarePenIcon
+          className={iconClass("SquarePen")}
+          onClick={() => handleIconClick("SquarePen")}
+        />
+        <Settings2
+          className={iconClass("Settings2")}
+          onClick={() => handleIconClick("Settings2")}
+        />
       </div>
     </>
   )
