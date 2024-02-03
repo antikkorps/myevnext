28a7eb6ca0352302f1a4c1b04895bd55283eaec6
Franck Vienot <68828345+antikkorps@users.noreply.github.com>
2024-02-03 18:42:27 +0100

    Update page.tsx
    added description

--- app/dashboard/clients/page.tsx
+++ app/dashboard/clients/page.tsx
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Clients() {
-  return <div>Ceci est la page Clients</div>
+  return (
+<>
+<div>Ceci est la page Clients</div>
+<p>ici le listing de l'ensemble des clients liés au compte client</p>
+</>
+)
 }
