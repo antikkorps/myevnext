ab8892e90dcc7d3d6de354451f02f87ff483b131
Franck <franckvienot7@gmail.com>
2024-02-04 18:00:38 +0100

    corrected clients page

--- app/dashboard/clients/page.tsx
+++ app/dashboard/clients/page.tsx
@@ -2,9 +2,9 @@ import React from "react"
 
 export default function Clients() {
   return (
-<>
-<div>Ceci est la page Clients</div>
-<p>ici le listing de l'ensemble des clients liés au compte client</p>
-</>
-)
+    <>
+      <div>Ceci est la page Clients</div>
+      <p>ici le listing de l&apos;ensemble des clients liés au compte client</p>
+    </>
+  )
 }
