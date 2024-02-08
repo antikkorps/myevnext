fe5ae0397f99fc79e942c261fc3f140559146167
Franck <franckvienot7@gmail.com>
2024-02-08 18:06:14 +0100

    added actions

---
+++ actions/get-company.server.ts
@@ -0,0 +1,19 @@
+interface Company {
+  id: string
+  name: string
+  address: string
+  city: string
+  zipCode: string
+}
+
+export async function getCompanies() {
+  try {
+    const response = await fetch(API_ENDPOINTS.POSTS)
+    const data: Company[] = await response.json()
+    console.log("Réponse de l'API:", data)
+    return data
+  } catch (error) {
+    console.error("Erreur lors de la récupération des posts:", error)
+    throw error
+  }
+}
