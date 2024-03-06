9942840760819f06756ee76f86fc04141fa95b0c
Franck <franckvienot7@gmail.com>
2024-03-06 14:04:32 +0100

    added a component on dashboard to test daisyUI

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-32 lg:py-52 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900 dark:text-white">
-            Dashboard
+            This is the Dashboard Layout
           </h2>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">{children}</div>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,15 @@ import React from "react"
 function Dashboard() {
   return (
     <>
-      <div className="sm:py-5">This is the Dashboard</div>
+      <div className="sm:py-5">This is the Dashboard page</div>
+
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-title">Total Page Views</div>
+          <div className="stat-value">89,400</div>
+          <div className="stat-desc">21% more than last month</div>
+        </div>
+      </div>
     </>
   )
 }
