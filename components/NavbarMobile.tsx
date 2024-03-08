2dbe2c66eb13bee31628fbd7f5abfb0188ef01cc
Franck <franckvienot7@gmail.com>
2024-03-08 11:38:43 +0100

    add avatar to NavbarMobile and if not connected link to login

--- components/NavbarMobile.tsx
+++ components/NavbarMobile.tsx
@@ -1,14 +1,26 @@
 "use client"
 import Link from "next/link"
 import { useState } from "react"
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { HomeIcon, User, SquarePenIcon, Settings2, Search } from "lucide-react"
+import { useAuth } from "@/app/auth-context"
+import { useRouter } from "next/navigation"
 
 export default function NavbarMobile() {
   const [selectedIcon, setSelectedIcon] = useState<String | null>(null)
+  const { user, checkUser, login, logout } = useAuth()
+  const router = useRouter()
 
   const handleIconClick = (iconName: String) => {
     setSelectedIcon(iconName)
+  }
+
+  const handleLogin = async () => {
+    try {
+      router.push("/auth/login")
+    } catch (error) {
+      console.error("An unexpected error happened:", error)
+    }
   }
 
   const iconClass = (iconName: String) =>
@@ -26,9 +38,20 @@ export default function NavbarMobile() {
         <Link href="/search">
           <Search className={iconClass("User")} onClick={() => handleIconClick("User")} />
         </Link>
-        <Link href="/profile">
-          <User className={iconClass("User")} onClick={() => handleIconClick("User")} />
-        </Link>
+
+        {user ? (
+          <>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </>
+        ) : (
+          <Link href="/profile">
+            <User className={iconClass("User")} onClick={handleLogin} />
+          </Link>
+        )}
+
         <Link href="/create">
           <SquarePenIcon
             className={iconClass("SquarePen")}
